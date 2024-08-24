b05927792dd1562d9e562c71c64e653e96ef9526
mordieh <mordieh.al.jabban@ogr.dpu.edu.tr>
2024-08-24 21:57:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container')
+
+function iphone(phone){
+    img.src = phone;
+}
+
+function background(color){
+    container.style.background = color;
+}
